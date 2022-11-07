56fc701ffe74907d1ce30a27a049c8d66758e9a0
LEMM <luismejia69@gmail.com>
2022-11-07 13:34:28 -0500

    setting actualizados

--- Back-end/CRUD_API/src/settings.py
+++ Back-end/CRUD_API/src/settings.py
@@ -1,17 +1,17 @@
 import os
-
 from dotenv import load_dotenv
 
-load_dotenv()
-
+load_dotenv(os.getenv('PATH_TO_ENV'))
 
 class Settings:
-    CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-    PORT = int(os.getenv("PORT", 8080))
-    FIREBASE_URL_ONE = os.getenv("FIREBASE_URL_ONE")
-    LOGIN_API_URL = os.getenv("LOGIN_API_URL")
-
-    ALLOW_ORIGINS = os.getenv("ALLOW_ORIGINS")
-    ALLOW_CREDENTIALS = os.getenv("ALLOW_CREDENTIALS")
-    ALLOW_METHODS = os.getenv("ALLOW_METHODS")
-    ALLOW_HEADERS = os.getenv("ALLOW_HEADERS")
+    FIREBASE_DATABASE: str =  os.getenv('FIREBASE_DB') if os.getenv('FIREBASE_DB') else None
+    MONGO_DATABASE: str =  os.getenv('MONGO_DB') if os.getenv('MONGO_DB') else None
+    DB_PORT: str = os.getenv('DB_PORT')
+    DB_NAME: str = os.getenv('DB_NAME')
+    LOGIN_API_URL: str = os.getenv("LOGIN_API_URL")
+    ALLOW_ORIGINS: str = os.getenv("ALLOW_ORIGINS")
+    ALLOW_CREDENTIALS: str = os.getenv("ALLOW_CREDENTIALS")
+    ALLOW_METHODS: str = os.getenv("ALLOW_METHODS")
+    ALLOW_HEADERS: str = os.getenv("ALLOW_HEADERS")
+    PORT: int = os.getenv("HOST_PORT")
+    HOST: str = os.getenv('HOST')
